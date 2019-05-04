c7fec8276c461952e6bac21a55bfbfce13cd3444
dev <dev@MacBook-Pro-de-Carlos.local>
2019-05-04 20:07:43 +0200

    basic user method

--- api/index.js
+++ api/index.js
@@ -2,26 +2,66 @@
 
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyparser = require('body-parser');
+const jsonBodyParser = bodyparser.json();
+const { Schema } = mongoose;
 
-mongoose.connect('mongodb://127.0.0.1:27017/notes-app',{ useNewUrlParser: true })
+mongoose.connect('mongodb://localhost:27017/notesapp', {useNewUrlParser: true})
     .then(() => {
 
         const app = express();
 
-        app.get('/api/index', (req,res) => {
-            res.json({status:'OK', message:'method get'});
+        const UserSchema = new Schema({
+            name : String,
+            surname : String
+        });
+        
+        const User = mongoose.model('user',UserSchema);
+
+        app.get('/api/list', (req,res) => {
+
+            User.find()
+            .then(users => res.json({status:'OK',list:users}));
+
         });
 
-        app.post('/api/index', (req,res) => {
-            res.json({status:'OK', message:'method post'});
+        app.get('/api/retrieve/:id', (req,res) => {
+
+            const id = req.params.id;
+
+            User.findById(id)
+            .then(user => res.json({status:'OK',user:user}));
+
         });
 
-        app.put('/api/index', (req,res) => {
-            res.json({status:'OK', message:'method put'});
+        app.post('/api/create', jsonBodyParser, (req,res) => {
+            
+            const name = req.body.name;
+            const surname = req.body.surname;
+
+            User.create({name,surname})
+            .then(user => res.json({status:'OK',user:user}));
+
         });
 
-        app.delete('/api/index', (req,res) => {
-            res.json({status:'OK', message:'method delete'});
+        app.put('/api/update/:id', jsonBodyParser, (req,res) => {
+            
+            const id = req.params.id;
+            const name = req.body.name;
+            const surname = req.body.surname;
+
+            User.findByIdAndUpdate({_id:id},{$set:{name,surname}})
+            .then(user => res.json({status:'OK',user:user}));
+
+        });
+
+        app.delete('/api/delete/:id', (req,res) => {
+            
+            const id = req.params.id;
+
+            User.findByIdAndRemove({_id:id})
+            .then(user => res.json({status:'OK',user:user}));
+            
         });
 
         app.listen(3000, () => console.log('server open in port 3000'));
@@ -30,5 +70,5 @@ mongoose.connect('mongodb://127.0.0.1:27017/notes-app',{ useNewUrlParser: true }
     .catch(err => {
         console.error('App started error');
         process.exit();
-        
+
     })
